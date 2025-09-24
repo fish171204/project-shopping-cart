dc23fe60fb7b7c38187930458bc270a0e3ec626a
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-24 13:12:30 +0700

    feat(logging): add detailed logging in recovery (path, method, IP, error, stack)

--- internal/middleware/recovery_middleware.go
+++ internal/middleware/recovery_middleware.go
@@ -1,17 +1,29 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
+	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 )
 
-func RecoveryMiddleware(httpLogger zerolog.Logger) gin.HandlerFunc {
+func RecoveryMiddleware(recoveryLogger zerolog.Logger) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 
 		defer func() {
 			if err := recover(); err != nil {
+				stack := debug.Stack()
+
+				recoveryLogger.Error().
+					Str("path", ctx.Request.URL.Path).
+					Str("method", ctx.Request.Method).
+					Str("client_ip", ctx.ClientIP()).
+					Str("panic", fmt.Sprintf("%v", err)).
+					Str("stack", string(stack)).
+					Msg("panic occoured")
+
 				ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
 					"code":    "INTERNAL_SERVER_ERROR",
 					"message": "Please try again later.",
