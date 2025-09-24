50a22011f50f5c311d63af6b009d057f1a7a16d6
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-24 11:33:31 +0700

    chore(config): copy and integrate database config

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,12 +5,31 @@ import (
 	"os"
 )
 
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
 type Config struct {
 	ServerAddress string
+	DB            DatabaseConfig
 }
 
 func NewConfig() *Config {
 	return &Config{
 		ServerAddress: fmt.Sprintf(":%s", os.Getenv("SERVER_PORT")),
+
+		DB: DatabaseConfig{
+			Host:     utils.GetEnv("DB_HOST", "localhost"),
+			Port:     utils.GetEnv("DB_PORT", "5432"),
+			User:     utils.GetEnv("DB_USER", "postgres"),
+			Password: utils.GetEnv("DB_PASSWORD", "postgres"),
+			DBName:   utils.GetEnv("DB_NAME", "myapp"),
+			SSLMode:  utils.GetEnv("DB_SSLMODE", "disable"),
+		},
 	}
 }
