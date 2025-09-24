5174d93f0ab168bda4f98dd55b8e772defb0325d
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-24 10:54:45 +0700

    refactor(repository): remove in-memory repository logic

--- internal/repository/interfaces.go
+++ internal/repository/interfaces.go
@@ -1,12 +1,10 @@
 package repository
 
-import "user-management-api/internal/models"
-
 type UserRepository interface {
-	FindAll() ([]models.User, error)
-	FindByUUID(uuid string) (models.User, bool)
-	FindByEmail(email string) (models.User, bool)
-	Create(user models.User) error
-	Update(uuid string, user models.User) error
-	Delete(uuid string) error
+	FindAll()
+	FindByUUID(uuid string)
+	FindByEmail(email string)
+	Create()
+	Update(uuid string)
+	Delete(uuid string)
 }
--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,71 +1,24 @@
 package repository
 
-import (
-	"fmt"
-	"slices"
-	"user-management-api/internal/models"
-)
-
 type InMemoryUserRepository struct {
-	users []models.User
 }
 
 func NewInMemoryUserRepository() UserRepository {
-	return &InMemoryUserRepository{
-		users: make([]models.User, 0),
-	}
+	return &InMemoryUserRepository{}
 }
 
 // GET
-func (ur *InMemoryUserRepository) FindAll() ([]models.User, error) {
-	return ur.users, nil
-}
+func (ur *InMemoryUserRepository) FindAll() {}
 
-func (ur *InMemoryUserRepository) FindByUUID(uuid string) (models.User, bool) {
-	for _, user := range ur.users {
-		if user.UUID == uuid {
-			return user, true
-		}
-	}
+func (ur *InMemoryUserRepository) FindByUUID(uuid string) {}
 
-	return models.User{}, false
-}
-
-func (ur *InMemoryUserRepository) FindByEmail(email string) (models.User, bool) {
-	for _, user := range ur.users {
-		if user.Email == email {
-			return user, true
-		}
-	}
-
-	return models.User{}, false
-}
+func (ur *InMemoryUserRepository) FindByEmail(email string) {}
 
 // POST
-func (ur *InMemoryUserRepository) Create(user models.User) error {
-	ur.users = append(ur.users, user)
-	return nil
-}
+func (ur *InMemoryUserRepository) Create() {}
 
 // PUT
-func (ur *InMemoryUserRepository) Update(uuid string, user models.User) error {
-	for i, u := range ur.users {
-		if u.UUID == uuid {
-			ur.users[i] = user
-			return nil
-		}
-	}
-
-	return fmt.Errorf("user not found")
-}
+func (ur *InMemoryUserRepository) Update(uuid string) {}
 
 // DELETE
-func (ur *InMemoryUserRepository) Delete(uuid string) error {
-	for i, u := range ur.users {
-		if u.UUID == uuid {
-			ur.users = slices.Delete(ur.users, i, i+1)
-			return nil
-		}
-	}
-	return fmt.Errorf("user not found")
-}
+func (ur *InMemoryUserRepository) Delete(uuid string) {}
