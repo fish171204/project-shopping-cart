e3169e1a1baaedbde24b53c7fe40ad95e6ded56f
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-25 14:41:14 +0700

    chore(logger): filter out non-application lines (Go runtime, debug)

--- internal/middleware/recovery_middleware.go
+++ internal/middleware/recovery_middleware.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
@@ -16,6 +17,7 @@ func RecoveryMiddleware(recoveryLogger *zerolog.Logger) gin.HandlerFunc {
 		defer func() {
 			if err := recover(); err != nil {
 				stack := debug.Stack()
+				stack_at := ExtractFirstAppStackLine(stack)
 
 				recoveryLogger.Error().
 					Str("path", ctx.Request.URL.Path).
@@ -23,6 +25,7 @@ func RecoveryMiddleware(recoveryLogger *zerolog.Logger) gin.HandlerFunc {
 					Str("client_ip", ctx.ClientIP()).
 					Str("panic", fmt.Sprintf("%v", err)).
 					Str("stack", string(stack)).
+					Str("stack_at", stack_at).
 					Msg("panic occoured")
 
 				ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
@@ -39,4 +42,15 @@ func RecoveryMiddleware(recoveryLogger *zerolog.Logger) gin.HandlerFunc {
 func ExtractFirstAppStackLine(stack []byte) string {
 	lines := bytes.Split(stack, []byte("\n"))
 
+	for _, line := range lines {
+		if bytes.Contains(line, []byte(".go")) &&
+			!bytes.Contains(line, []byte("/runtime/")) &&
+			!bytes.Contains(line, []byte("/debug/")) &&
+			!bytes.Contains(line, []byte("/recovery_middleware.go/")) {
+			cleanLine := strings.TrimSpace(string(line))
+			return cleanLine
+		}
+	}
+
+	return ""
 }
