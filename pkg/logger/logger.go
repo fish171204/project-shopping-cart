88395b01cca89e9051bc976c517914389936c034
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-25 13:43:28 +0700

    feat(logger): integrate Lumberjack for file logging

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,6 +1,9 @@
 package logger
 
 import (
+	"io"
+
+	"github.com/natefinch/lumberjack"
 	"github.com/rs/zerolog"
 )
 
@@ -14,8 +17,18 @@ type LoggerConfig struct {
 }
 
 func NewLogger(config LoggerConfig) *zerolog.Logger {
+	var writer io.Writer
 
-	logger := zerolog.New().With().Timestamp().Logger()
+	writer = &lumberjack.Logger{
+		Filename:   "../../internal/logs/http.log",
+		MaxSize:    1,    // MB
+		MaxBackups: 5,    // number of backup files
+		MaxAge:     5,    // days before deletion
+		Compress:   true, // disabled by default (compress)
+		LocalTime:  true, // use local time in log
+	}
+
+	logger := zerolog.New(writer).With().Timestamp().Logger()
 
 	return &logger
 }
