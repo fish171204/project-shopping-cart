d7ad93ae136cbfb0e2f51959051674a17b7e885e
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-27 18:22:18 +0700

    feat(mapping): add created_at to output DTO with time formatting

--- internal/dto/v1/user_dto.go
+++ internal/dto/v1/user_dto.go
@@ -7,12 +7,13 @@ import (
 
 // Data Transfer Object (DTO): chuyển dữ liệu ra response
 type UserDTO struct {
-	UUID   string `json:"uuid"`
-	Name   string `json:"full_name"`
-	Email  string `json:"email_address"`
-	Age    int    `json:"age"`
-	Status string `json:"status"`
-	Level  string `json:"level"`
+	UUID      string `json:"uuid"`
+	Name      string `json:"full_name"`
+	Email     string `json:"email_address"`
+	Age       int    `json:"age"`
+	Status    string `json:"status"`
+	Level     string `json:"level"`
+	CreatedAt string `json:"created_at`
 }
 
 type CreateUserInput struct {
@@ -52,11 +53,12 @@ func (input *UpdateUserInput) MapUpdateInputToModel() {
 // Response
 func MapUserToDTO(user sqlc.User) *UserDTO {
 	dto := &UserDTO{
-		UUID:   user.UserUuid.String(),
-		Name:   user.UserFullname,
-		Email:  user.UserEmail,
-		Status: mapStatusText(int(user.UserStatus)),
-		Level:  mapLevelText(int(user.UserLevel)),
+		UUID:      user.UserUuid.String(),
+		Name:      user.UserFullname,
+		Email:     user.UserEmail,
+		Status:    mapStatusText(int(user.UserStatus)),
+		Level:     mapLevelText(int(user.UserLevel)),
+		CreatedAt: user.UserCreatedAt.Format("2006-01-02 15:04:05"),
 	}
 
 	if user.UserAge != nil {
