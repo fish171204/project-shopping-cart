bc5a91165e2761f8e3a5e207eff6c48f157a7676
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-28 18:31:22 +0700

    feat(middleware): extract Trace ID from X-Trace-ID request header

--- internal/middleware/trace_middleware.go
+++ internal/middleware/trace_middleware.go
@@ -1,10 +1,16 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 func TraceMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
+		traceID := ctx.GetHeader("X-Trace-Id")
+		if traceID == "" {
+			traceID = uuid.New().String()
+		}
 		ctx.Next()
 
 	}
