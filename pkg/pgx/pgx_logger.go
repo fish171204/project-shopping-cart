0190a1b011a703018366fe81d0ae0516c92d6d56
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-28 17:04:24 +0700

    refactor(sql-tracer): move logger builder into temporary variable

--- pkg/pgx/pgx_logger.go
+++ pkg/pgx/pgx_logger.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/jackc/pgx/v5/tracelog"
 	"github.com/rs/zerolog"
-	"gorm.io/gorm/logger"
 )
 
 type PgxZerologTracer struct {
@@ -22,11 +21,19 @@ func (t *PgxZerologTracer) Log(ctx context.Context, level tracelog.LogLevel, msg
 	args, _ := data["args"].([]any)
 	duration, _ := data["time"].(time.Duration)
 
-	baseLogger := t.Logger.With()
+	baseLogger := t.Logger.With().
+		Dur("duration", duration).
+		Str("sql", sql).
+		Interface("args", args)
+
+	logger := baseLogger.Logger()
 
 	if msg == "Query" && duration > t.SlowQueryLimit {
 		logger.Warn().Str("event", "Query").Msg("Slow SQL Query")
 		return
 	}
 
+	if msg == "Query" {
+		logger.Info().Str("event", "Query").Msg("Executed SQL")
+	}
 }
