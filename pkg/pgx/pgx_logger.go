180589035da938a0facaa624234fbd11ee9b69dd
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-28 16:49:20 +0700

    add params to business tracer

--- pkg/pgx/pgx_logger.go
+++ pkg/pgx/pgx_logger.go
@@ -2,11 +2,15 @@ package pgx
 
 import (
 	"context"
+	"time"
 
 	"github.com/jackc/pgx/v5/tracelog"
+	"github.com/rs/zerolog"
 )
 
 type PgxZerologTracer struct {
+	Logger         zerolog.Logger
+	SlowQueryLimit time.Duration
 }
 
 func (t *PgxZerologTracer) Log(ctx context.Context, level tracelog.LogLevel, msg string, data map[string]any)
