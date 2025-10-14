2647d776dda920e5b15b591edc55b19bf656414b
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-10-14 16:48:17 +0700

    refactor code

--- internal/utils/pagination.go
+++ internal/utils/pagination.go
@@ -1,20 +1,20 @@
 package utils
 
-import "math"
-
 type Pagination struct {
-	Page        int32 `json:page`
-	Limit       int32 `json:limit`
-	TotalRecord int32 `json:total_records`
-	TotalPage   int32 `json:total_pages`
-	HasNext     bool  `json:has_next`
-	HasPrev     bool  `json:has_prev`
+	Page        int32 `json:"page"`
+	Limit       int32 `json:"limit"`
+	TotalRecord int32 `json:"total_records"`
+	TotalPage   int32 `json:"total_pages"`
+	HasNext     bool  `json:"has_next"`
+	HasPrev     bool  `json:"has_prev"`
 }
 
 func NewPagination(page, limit, totalRecords int32) *Pagination {
 
 	// Tổng số trang = ceil(tổng số record / số phần tử trên 1 trang)
-	totalPages := math.Ceil(float64(totalRecords) / float64(limit))
+	// C1: totalPages := math.Ceil(float64(totalRecords) / float64(limit))
+	// C2: int
+	totalPages := (totalRecords + limit - 1) / limit
 
 	return &Pagination{
 		Page:        page,
