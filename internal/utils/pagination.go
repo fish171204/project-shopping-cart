f86448115f7e6871962aa609ff515cd82ce3d184
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-10-14 16:45:02 +0700

    feat(response): Add logic to calculate TotalPage using integer division trick

--- internal/utils/pagination.go
+++ internal/utils/pagination.go
@@ -1,5 +1,7 @@
 package utils
 
+import "math"
+
 type Pagination struct {
 	Page        int32 `json:page`
 	Limit       int32 `json:limit`
@@ -7,4 +9,19 @@ type Pagination struct {
 	TotalPage   int32 `json:total_pages`
 	HasNext     bool  `json:has_next`
 	HasPrev     bool  `json:has_prev`
+}
+
+func NewPagination(page, limit, totalRecords int32) *Pagination {
+
+	// Tổng số trang = ceil(tổng số record / số phần tử trên 1 trang)
+	totalPages := math.Ceil(float64(totalRecords) / float64(limit))
+
+	return &Pagination{
+		Page:        page,
+		Limit:       limit,
+		TotalRecord: totalRecords,
+		TotalPage:   totalPages,
+		HasNext:     page < int32(totalPages),
+		HasPrev:     page > 1,
+	}
 }
