5a6a29475032126932c4b093ce188c935069625d
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-10-16 18:04:38 +0700

    feat(cache): implement Set() to store data in cache (JSON marshal)

--- pkg/cache/redis.go
+++ pkg/cache/redis.go
@@ -1,12 +1,33 @@
 package cache
 
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
 type RedisCacheService struct {
+	ctx context.Context
+	rdb *redis.Client
 }
 
-func NewRedisCacheService() *RedisCacheService {
-	return &RedisCacheService{}
+func NewRedisCacheService(rdb *redis.Client) *RedisCacheService {
+	return &RedisCacheService{
+		ctx: context.Background(),
+		rdb: rdb,
+	}
 }
 
 func (cs *RedisCacheService) Get() {
 
+}
+
+func (cs *RedisCacheService) Set(key string, value any, ttl time.Duration) error {
+	data, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	return cs.rdb.Set(cs.ctx, key, data, 0).Err()
 }
