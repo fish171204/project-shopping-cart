0cb02a9b0ea41a7185ec673e23de7d33f67a2e5d
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-10-16 22:00:38 +0700

    feat(cors): handle preflight requests (OPTIONS method)

--- internal/middleware/cors_middleware.go
+++ internal/middleware/cors_middleware.go
@@ -9,6 +9,7 @@ import (
 func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-API-Key, Authorization")
 
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.AbortWithStatus(http.StatusNoContent)
