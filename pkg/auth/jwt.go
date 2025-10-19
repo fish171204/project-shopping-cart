c6c44781e885cec9ffbc9d2326438df1d991e2e1
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-10-19 23:12:05 +0700

    feat(jwt): Start structuring JWT Service

---
+++ pkg/auth/jwt.go
@@ -0,0 +1,16 @@
+package auth
+
+type JWTService struct {
+}
+
+func NewJWTService() *JWTService {
+	return &JWTService{}
+}
+
+func (js *JWTService) GenerateAccessToken() {
+
+}
+
+func (js *JWTService) GenerateRefreshToken() {
+
+}
