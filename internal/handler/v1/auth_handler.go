f1459819a090c870bbadaeec0d5579f4c8e4a981
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-10-19 22:10:59 +0700

    feat(auth): Create Auth DTO to receive Email and Password input

--- internal/handler/v1/auth_handler.go
+++ internal/handler/v1/auth_handler.go
@@ -1,7 +1,11 @@
 package v1handler
 
 import (
+	"net/http"
+	v1dto "user-management-api/internal/dto/v1"
 	v1service "user-management-api/internal/service/v1"
+	"user-management-api/internal/utils"
+	"user-management-api/internal/validation"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,9 +21,21 @@ func NewAuthHandler(service v1service.AuthService) *AuthHandler {
 }
 
 func (ah *AuthHandler) Login(ctx *gin.Context) {
+	var input v1dto.LoginInput
+	if err := ctx.ShouldBindQuery(&input); err != nil {
+		utils.ResponseValidator(ctx, validation.HandleValidationErrors(err))
+		return
+	}
 
+	err := ah.service.Login(ctx, input.Email, input.Password)
+	if err != nil {
+		utils.ResponseError(ctx, err)
+	}
+
+	utils.ResponseSuccess(ctx, http.StatusOK, "Login successfully")
 }
 
 func (ah *AuthHandler) Logout(ctx *gin.Context) {
+	utils.ResponseSuccess(ctx, http.StatusOK, "Logout successfully")
 
 }
