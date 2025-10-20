53c045e4094e249a3f4d6ac0a91524c30b8e9fce
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-10-20 21:54:54 +0700

    feat(auth): validate Authorization Header

---
+++ internal/middleware/auth_middleware.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" || strings.HasPrefix(authHeader, "Bearer ") {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "Authorization header missing or invalid",
+			})
+		}
+
+		ctx.Next()
+	}
+}
