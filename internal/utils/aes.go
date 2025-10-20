e6c7c5db43482b2b3be9cf5bcdbe4b53b5273020
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-10-20 21:25:29 +0700

    feat(crypto): add DecryptAES function for next lesson

--- internal/utils/aes.go
+++ internal/utils/aes.go
@@ -30,3 +30,27 @@ func EncryptAES(plainText []byte, key []byte) (string, error) {
 
 	return base64.URLEncoding.EncodeToString(cipherText), nil
 }
+
+// Giải mã truyền string trả về []byte
+func DecryptAES(cipherBase64 string, key []byte) ([]byte, error) {
+	cipherText, err := base64.URLEncoding.DecodeString(cipherBase64)
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := aesGCM.NonceSize()
+	nonce, cipherText := cipherText[:nonceSize], cipherText[nonceSize:]
+
+	// Open (giải mã)
+	return aesGCM.Open(nil, nonce, cipherText, nil)
+}
